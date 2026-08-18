d1b5dc62a8160a0f6becb3246925c9ee98c03e11
NahuelGarciaDelta <nahuel.garcia@deltamining.com.ar>
2026-08-18 15:27:44 -0300

    Activar filtro de proyectos en Bienvenida

--- src/modules/home/index.js
+++ src/modules/home/index.js
@@ -1,3 +1,3 @@
-export { default as ViewBienvenida } from "./ViewBienvenida.jsx";
+export { default as ViewBienvenida } from "./ViewBienvenidaProjectFilter.jsx";
 
 export { default as ExecutiveDashboard } from "./ExecutiveDashboard.jsx";
