2580e28ef3fc33cc64f150fc6034cbd8f9b44154
NahuelGarciaDelta <nahuel.garcia@deltamining.com.ar>
2026-08-19 10:37:01 -0300

    Corregir globals del navegador en validación ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -10,12 +10,14 @@ export default [
       parserOptions: { ecmaFeatures: { jsx: true } },
       globals: {
         window: "readonly", document: "readonly", localStorage: "readonly", sessionStorage: "readonly",
-        fetch: "readonly", URL: "readonly", URLSearchParams: "readonly", Blob: "readonly", DOMParser: "readonly", navigator: "readonly",
+        fetch: "readonly", Request: "readonly", Response: "readonly", Headers: "readonly",
+        URL: "readonly", URLSearchParams: "readonly", Blob: "readonly", DOMParser: "readonly", navigator: "readonly",
         FileReader: "readonly", FormData: "readonly", setTimeout: "readonly", clearTimeout: "readonly",
         setInterval: "readonly", clearInterval: "readonly", console: "readonly", structuredClone: "readonly",
         AbortController: "readonly", CustomEvent: "readonly", Event: "readonly", Worker: "readonly",
-        performance: "readonly", requestAnimationFrame: "readonly", cancelAnimationFrame: "readonly",
-        indexedDB: "readonly", self: "readonly", process: "readonly"
+        MutationObserver: "readonly", ResizeObserver: "readonly", IntersectionObserver: "readonly",
+        caches: "readonly", indexedDB: "readonly", self: "readonly", process: "readonly",
+        requestAnimationFrame: "readonly", cancelAnimationFrame: "readonly"
       }
     },
     rules: {
