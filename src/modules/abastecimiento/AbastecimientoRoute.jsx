cfac77849b46d3bfbfefc3f73297351b5654d997
NahuelGarciaDelta <nahuel.garcia@deltamining.com.ar>
2026-09-14 14:17:55 -0300

    Restore OPS solicitud deletion route parity

--- src/modules/abastecimiento/AbastecimientoRoute.jsx
+++ src/modules/abastecimiento/AbastecimientoRoute.jsx
@@ -1,6 +1,16 @@
-import React from "react";
-import { AbastecimientoModule } from "./AbastecimientoModule.jsx";
+import React,{useState} from "react";
+import {PageLoadingMotoniveladora} from "../../components/ui/index.jsx";
+import DeleteSolicitudByNumber from "./DeleteSolicitudByNumber.jsx";
 
-export default function AbastecimientoRoute(props) {
-  return <AbastecimientoModule {...props} />;
+const LazyAbastecimientoModule=React.lazy(()=>
+  import("./AbastecimientoModule.jsx").then((module)=>({default:module.default||module.AbastecimientoModule})),
+);
+
+export default function AbastecimientoRoute(props){
+  const [refreshKey,setRefreshKey]=useState(0);
+  const showDeleteSolicitud=!props.readOnly&&props.initialTab==="solicitudes";
+  return <React.Suspense fallback={<PageLoadingMotoniveladora label="Cargando Abastecimiento..."/>}>
+    {showDeleteSolicitud&&<DeleteSolicitudByNumber deps={props.deps} onDeleted={()=>setRefreshKey(value=>value+1)}/>} 
+    <LazyAbastecimientoModule key={refreshKey} {...props}/>
+  </React.Suspense>;
 }
